a614bf957de9226949656584ff1ff29713033e97
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-06-24 16:38:33 +0000

    fix: cover lose rotation when paused
    Signed-off-by: KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>

--- platform-common/src/client/kotlin/org/lolicode/moemusic/platform/client/ui/NowPlayingHud.kt
+++ platform-common/src/client/kotlin/org/lolicode/moemusic/platform/client/ui/NowPlayingHud.kt
@@ -196,8 +196,8 @@ object NowPlayingHud {
         layout: Layout,
         textureId: ResourceLocation,
     ) {
-        val shouldSpin = config.spinCover && ctx.state is PlaybackState.Playing
-        if (!shouldSpin) {
+        val shouldRotate = config.spinCover && ctx.state !is PlaybackState.Stopped
+        if (!shouldRotate) {
             gfx.blit(
                 textureId,
                 layout.coverX,
